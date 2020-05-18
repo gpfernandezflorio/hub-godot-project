12544fbe7e6df9be1f104b2a119bdbe9bc035cd6
Gonzalo Pablo Fernandez <gpfernandezflorio@gmail.com>
2020-05-18 12:33:36 -0300

    Más consideraciones al ejecutar la versión compilada sin userFS

--- INIT.gd
+++ INIT.gd
@@ -8,17 +8,19 @@ var instrucciones = \
 	'y vuelva a intentarlo.\n\nPresione cuaquier tecla para salir.'
 
 func _ready():
-	if Globals.get("userfs"):
+	var userfs = Globals.get("userfs")
+	if userfs:
 		ruta_raiz = OS.get_data_dir() + "/"
-	var ruta_al_HUB = ruta_raiz + "src/HUB.gd"
-	if not (Globals.get("userfs") or OS.is_debug_build()):
-		ruta_al_HUB += "c"
+	var extension = ".gd"
+	if not (userfs or OS.is_debug_build()):
+		extension += "c"
+	var ruta_al_HUB = ruta_raiz + "src/HUB" + extension
 	if File.new().file_exists(ruta_al_HUB):
 		var HUB = Node.new()
 		HUB.set_name("HUB")
 		add_child(HUB)
 		HUB.set_script(load(ruta_al_HUB))
-		if not HUB.inicializar(HUB):
+		if not HUB.inicializar(HUB, extension):
 			HUB.queue_free()
 			error('Hubo un error al inicializar el HUB.' + \
 			instrucciones)
