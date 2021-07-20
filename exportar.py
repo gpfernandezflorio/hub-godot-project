35fd9196e55f275023cc1431c02a78e87461f133
Ralo <gpfernandezflorio@gmail.com>
2021-07-20 12:55:35 -0300

    Adapto la escena principal a Godot 3

--- exportar.py
+++ exportar.py
@@ -1,17 +1,37 @@
 import os
 
+reemplazos_tscn = [
+    {'2':'[gd_scene load_steps=2 format=1]\n', '3':'[gd_scene load_steps=2 format=2]\n'},
+    {'2':'\nscript/script = ExtResource( 1 )\n', '3':'\nscript = ExtResource( 1 )\n'}
+]
+archivo_tscn = 'HUB.tscn'
+
 def ajustar_a_version_godot(major):
-  target = '#@{m}'.format(m=major)
-  other = ('#@2' if major == 3 else '#@3')
-  ajustar_a_version_godot_recursivo('.', target, other)
+    target = str(major)
+    other = ('2' if major == 3 else '3')
+    ajustar_a_version_godot_tscn(target, other)
+    target = '#@' + target
+    other = '#@' + other
+    ajustar_a_version_godot_recursivo('.', target, other)
+
+def ajustar_a_version_godot_tscn(target, other):
+    f = open(archivo_tscn, 'r')
+    data = f.read()
+    f.close()
+    for r in reemplazos_tscn:
+        data = data.replace(r[other], r[target])
+    f = open(archivo_tscn, 'w')
+    f.write(data)
+    f.close()
+
 
 def ajustar_a_version_godot_recursivo(carpeta, target, other):
-  for archivo in os.listdir(carpeta):
-      ruta = os.path.join(carpeta, archivo)
-      if os.path.isdir(ruta):
-          ajustar_a_version_godot_recursivo(ruta, target, other)
-      elif es_archivo_a_modificar(ruta):
-          ajustar_a_version_godot_archivo(ruta, target, other)
+    for archivo in os.listdir(carpeta):
+        ruta = os.path.join(carpeta, archivo)
+        if os.path.isdir(ruta):
+            ajustar_a_version_godot_recursivo(ruta, target, other)
+        elif es_archivo_a_modificar(ruta):
+            ajustar_a_version_godot_archivo(ruta, target, other)
 
 def es_archivo_a_modificar(ruta):
     if not os.path.isfile(ruta):
