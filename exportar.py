161430dbb8247d2b50e712e77dc713e25c3f9281
Ralo <gpfernandezflorio@gmail.com>
2021-07-20 14:18:21 -0300

    Avanzo con la transición a Godot 3

--- exportar.py
+++ exportar.py
@@ -1,17 +1,18 @@
 import os
 
 reemplazos_tscn = [
-    {'2':'[gd_scene load_steps=2 format=1]\n', '3':'[gd_scene load_steps=2 format=2]\n'},
-    {'2':'\nscript/script = ExtResource( 1 )\n', '3':'\nscript = ExtResource( 1 )\n'}
+    {'#@2':'[gd_scene load_steps=2 format=1]\n', '#@3':'[gd_scene load_steps=2 format=2]\n'},
+    {'#@2':'\nscript/script = ExtResource( 1 )\n', '#@3':'\nscript = ExtResource( 1 )\n'}
 ]
 archivo_tscn = 'HUB.tscn'
+reemplazos_gd = [
+    {'#@2':'set_pos(', '#@3':'set_position('}
+]
 
 def ajustar_a_version_godot(major):
-    target = str(major)
-    other = ('2' if major == 3 else '3')
+    target = '#@{m}'.format(m=major)
+    other = ('#@2' if major == 3 else '#@3')
     ajustar_a_version_godot_tscn(target, other)
-    target = '#@' + target
-    other = '#@' + other
     ajustar_a_version_godot_recursivo('.', target, other)
 
 def ajustar_a_version_godot_tscn(target, other):
@@ -43,20 +44,20 @@ def ajustar_a_version_godot_archivo(ruta, target, other):
     f = open(ruta, 'r')
     data = f.read()
     f.close()
-    modificado = False
     resultado = []
     for l in data.split('\n'):
         if l.endswith(target) and l.startswith('#'):
             resultado.append(l[1:])
-            modificado = True
         elif l.endswith(other) and not l.startswith('#'):
             resultado.append('#{l}'.format(l=l))
-            modificado = True
         else:
             resultado.append(l)
-    if modificado:
+    resultado = '\n'.join(resultado)
+    for r in reemplazos_gd:
+        resultado = resultado.replace(r[other], r[target])
+    if resultado != data:
         f = open(ruta, 'w')
-        f.write('\n'.join(resultado))
+        f.write(resultado)
         f.close()
 
 def main():
